da3f8a136b68b6344edcd5bb0a8d74c8fbc5c792
Alan Gorton <8091018+alangorton@users.noreply.github.com>
2022-03-17 10:10:59 +0000

    moves styles out of App

--- FSAInfinity/App.tsx
+++ FSAInfinity/App.tsx
@@ -14,13 +14,14 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+import { styles } from './App.styles';
 
 function App(): ReactElement {
   const isDarkMode = useColorScheme() === 'dark';
@@ -47,24 +48,5 @@ function App(): ReactElement {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
