4945621334886fb4e867bc4496e2e42523a0f550
Alan Gorton <8091018+alangorton@users.noreply.github.com>
2022-03-23 11:30:19 +0000

    adds some content to detail screen (not formatted)

--- src/screens/AuthorityDetailScreen/index.tsx
+++ src/screens/AuthorityDetailScreen/index.tsx
@@ -1,21 +1,56 @@
 import React, { ReactElement, useLayoutEffect } from 'react';
 
-import { Text } from 'react-native';
+import { FlatList, ListRenderItem, Text, View } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { AuthorityDetailRoute, RootStackNavigation } from 'src/navigation';
+
+type Rating = {
+    readonly label: String;
+    readonly value: String;
+};
+
+function getRatings(): Rating[] {
+    return [
+        { label: '1', value: '55%' },
+        { label: '2', value: '55%' },
+        { label: '3', value: '55%' },
+        { label: '4', value: '55%' },
+        { label: '5', value: '55%' },
+        { label: 'Pass', value: '55%' },
+        { label: 'Exempt', value: '55%' },
+    ];
+}
 
 function AuthorityDetailScreen(): ReactElement {
     const route = useRoute<AuthorityDetailRoute>();
     const navigation = useNavigation<RootStackNavigation>();
 
-    const { name } = route.params.authority;
+    const { name: authorityName } = route.params.authority;
+
+    const title = `${authorityName} - Ratings`;
+    const ratings = getRatings();
 
     useLayoutEffect(() => {
-        navigation.setOptions({ title: name });
+        navigation.setOptions({ title: authorityName });
     });
 
-    return <Text>{name}</Text>;
+    let renderItem: ListRenderItem<Rating> = ({ item: rating }) => {
+        const { label, value } = rating;
+
+        return (
+            <Text>
+                {label} - {value}
+            </Text>
+        );
+    };
+
+    return (
+        <View>
+            <Text>{title}</Text>
+            <FlatList data={ratings} renderItem={renderItem} />
+        </View>
+    );
 }
 
 export { AuthorityDetailScreen };
